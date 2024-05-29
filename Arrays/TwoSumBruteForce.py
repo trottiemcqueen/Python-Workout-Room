9c1be76d7e62cf8af18fdb7999a54e3338ab0981
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-05-28 20:58:44 -0400

    added two sum solution in js now interpreting to python for practice

---
+++ Arrays/TwoSumBruteForce.py
@@ -0,0 +1,24 @@
+# In JavaScript
+# Two Sum brute force
+
+#const numsArray = [1, 3, 7, 9, 2];
+#const targetToFind = 11;
+
+#const findTwoSum = function (nums, target) {
+#  for (let p1 = 0; p1 < nums.length; p1++) {
+#    const numberToFind = target - nums[p1];
+#   for (let p2 = p1 + 1; p2 < nums.length; p2++) {
+#      if (numberToFind === nums[p2]) {
+#        return [p1, p2];
+#      }
+#    }
+#  }
+
+#  return null;
+#};
+
+#console.log(findTwoSum(numsArray, targetToFind));
+
+# I wrote this out in JavaScript, now attempting to interpret in Python, which
+# is my weaker language I'm working on to get stronger
+
--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 <hr>
 <hr>
 
-
+### Arrays
+  <ul>
+    <li>How to approach a problem(html)</li>
+    <li>Two Sum Brute Force(py)</li>
+    <li>Space Time Complexity(py)</li>
+  </ul>
 
 ### Data Structures and Algorithms
 
