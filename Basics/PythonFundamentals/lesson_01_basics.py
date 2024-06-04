d2368d8d4b70ed358b829ca205cc839f8cd81a4f
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-04 14:47:37 -0400

    added a new folder for basics and updated README

---
+++ Basics/PythonFundamentals/lesson_01_basics.py
@@ -0,0 +1,61 @@
+greeting = 'hello world!'
+print(greeting)
+
+# Variables
+name001 = 'Trottie' # name001 is the variable - assisgn a value
+_name001 = 'Trot' # can not start variable with numbers
+print(name001, _name001)
+
+# Fun
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line01)
+print(line01)
+
+# Python Operators
+  # Assignment operator '='
+  # Arithmatic op '+', '-', '/', '%', '**', '*', '//' plus more we will run into soon
+  
+multipli = 2 * 2
+print(multipli)
+
+subtr = 9 - 5
+print(subtr)
+
+divi = 100 / 2
+print(divi)
+
+modround = 24 % 5
+print(modround)
+
+anotherdiv = 200 // 3
+print(anotherdiv)
+
+tothepower = 2 ** 3
+print(tothepower)
+
+  # Assign variables to operators as well
+meaning = 99
+print(meaning)
+
+meaning += 1
+print(meaning)
+
+meaning -= 10
+print(meaning)
+
+meaning *= 20
+print(meaning)
+
+meaning /= 2
+print(meaning)
+
+meaning = round(meaning)
+print(meaning)
+
--- README.md
+++ README.md
@@ -3,16 +3,31 @@
 <hr>
 <hr>
 
-### Arrays
+### Basics
   <ul>
+    <ol>Python Fundamentals
+      <li>lesson_01_basics.py</li>
+      <li>Notes.html</li>
+    </ol>
     <li>How to approach a problem(html)</li>
     <li>Two Sum Brute Force(py)</li>
     <li>Space Time Complexity(py)</li>
   </ul>
 
 ### Data Structures and Algorithms
-
+  <ul>
+    <ol>Algo
+      <li>Algo</li>
+    </ol>
+    <li>linear_search.py</li>
+    <li>merge_sort.py</li>
+    <li>quicksort.py</li>
+    <li>recursion.py</li>
+    <li>sorting.py</li>
+  </ul>
+  
 <hr>
 
 ### Build / Playground
-
+  <li>encryption.py</li>
+  <li>path-finder.py</li>
