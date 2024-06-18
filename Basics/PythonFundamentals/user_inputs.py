4f092cebee48652137fb84ad4d7428c1e40ebfbd
Trottie McQueen <mcqueen.trottie007@gmail.com>
2024-06-18 18:58:10 -0400

    utilizing conditionals and inputs

--- Basics/PythonFundamentals/user_inputs.py
+++ Basics/PythonFundamentals/user_inputs.py
@@ -6,3 +6,16 @@ value = input("Please enter a value:\n")
 
 
 print(value)
+
+import sys
+import random
+
+print("")
+playerchoice = input("Enter ... \n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+player = int(playerchoice)
+
+if player < 1 | player > 3:
+    sys.exit("You myst enter 1, 2, or 3")
+    
+computerchoice = random.choice("123")
+computer = int(computerchoice)
